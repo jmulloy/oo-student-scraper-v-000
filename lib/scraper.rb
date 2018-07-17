50e36a92b0760aecb38533b27c7d87b238777178
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-17 16:25:26 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,11 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_url= Nokogiri::HTML(open("./fixtures/student-site/index"))
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
